b799486090ca8a9d293d2b3766d519206047e654
Suhel <suhel7822@gmail.com>
2025-12-23 22:34:12 +0530

    changed auth card css

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -2,7 +2,7 @@ import Logo from "../navbar/Logo";
 
 const Header = ({ label }: { label: string }) => {
   return (
-    <div className="flex flex-col gap-6">
+    <div className="flex flex-col items-start gap-6">
       <Logo isMobile={false} />
       <div className="flex w-full flex-col gap-1">
         <h1 className="h2-semibold font-space-grotesk">{label}</h1>
