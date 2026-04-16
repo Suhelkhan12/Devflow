1ee0b50b210536c28a9f2251f5e2b58da024fc41
Suhel <suhel7822@gmail.com>
2026-04-16 22:31:02 +0530

    deleted radio filters implementing it using tabs now

--- components/profile/profile-question-answers.tsx
+++ components/profile/profile-question-answers.tsx
@@ -1,10 +1,26 @@
-"use client";
-import RadioFilter from "../filters/radio-filter";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 
 const ProfileQuestionsAnswers = () => {
   return (
     <section className="mt-10">
-      <RadioFilter />
+      <Tabs defaultValue="questions">
+        <TabsList className="background-light800_dark300 h-auto! overflow-hidden p-0">
+          <TabsTrigger
+            value="questions"
+            className="data-[state=active]:bg-primary-fade dark:data-[state=active]:bg-dark-400 cursor-pointer rounded-none px-6 py-3"
+          >
+            Questions
+          </TabsTrigger>
+          <TabsTrigger
+            value="answers"
+            className="data-[state=active]:bg-primary-fade dark:data-[state=active]:bg-dark-400 cursor-pointer rounded-none px-6 py-3"
+          >
+            Answers
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="questions">Questions</TabsContent>
+        <TabsContent value="answers">Answers</TabsContent>
+      </Tabs>
     </section>
   );
 };
