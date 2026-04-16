228462d31e4bdbdce68054e4bcea54d19d0f386f
Suhel <suhel7822@gmail.com>
2026-04-16 22:39:48 +0530

    added suspense and state so that tabs dont fetch on page loads

--- components/profile/profile-question-answers.tsx
+++ components/profile/profile-question-answers.tsx
@@ -1,25 +1,30 @@
+"use client";
+
+import { Suspense, useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 
 const ProfileQuestionsAnswers = () => {
+  const [activeTab, setActiveTab] = useState<"questions" | "answers">("questions");
+
   return (
     <section className="mt-10">
-      <Tabs defaultValue="questions">
+      <Tabs value={activeTab} onValueChange={(val) => setActiveTab(val as "questions" | "answers")}>
         <TabsList className="background-light800_dark300 h-auto! overflow-hidden p-0">
           <TabsTrigger
             value="questions"
-            className="data-[state=active]:bg-primary-fade dark:data-[state=active]:bg-dark-400 cursor-pointer rounded-none px-6 py-3"
+            className="data-[state=active]:bg-primary-fade dark:data-[state=active]:bg-dark-400 text-primary-500 cursor-pointer rounded-none px-6 py-3 shadow-none"
           >
             Questions
           </TabsTrigger>
           <TabsTrigger
             value="answers"
-            className="data-[state=active]:bg-primary-fade dark:data-[state=active]:bg-dark-400 cursor-pointer rounded-none px-6 py-3"
+            className="data-[state=active]:bg-primary-fade dark:data-[state=active]:bg-dark-400 text-primary-500 cursor-pointer rounded-none px-6 py-3 shadow-none"
           >
             Answers
           </TabsTrigger>
         </TabsList>
-        <TabsContent value="questions">Questions</TabsContent>
-        <TabsContent value="answers">Answers</TabsContent>
+        <Suspense>{activeTab === "questions" && <TabsContent value={"questions"}>Questions</TabsContent>}</Suspense>
+        <Suspense>{activeTab === "answers" && <TabsContent value={"answers"}>Answers</TabsContent>}</Suspense>
       </Tabs>
     </section>
   );
